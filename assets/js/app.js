acfe6fc37d8118234340435d74e5264214fcb86c
labibj <ljamal2011@gmail.com>
2025-07-27 20:27:11 +0500

    video, audio tags, time - date picker

--- assets/js/app.js
+++ assets/js/app.js
@@ -39,3 +39,39 @@ const swiper = new Swiper('.swiper', {
     }
   }
 });
+
+
+$(function() {
+  $('input[name="daterange"]').daterangepicker({
+    opens: 'left'
+  }, function(start, end, label) {
+    console.log("A new date selection was made: " + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
+  });
+});
+
+
+var today = new Date();
+var minDate = today.setDate(today.getDate() + 1);
+
+$('#datePicker').datetimepicker({
+  useCurrent: false,
+  format: "MM/DD/YYYY",
+  minDate: minDate
+});
+
+var firstOpen = true;
+var time;
+
+$('#timePicker').datetimepicker({
+  useCurrent: false,
+  format: "hh:mm A"
+}).on('dp.show', function() {
+  if(firstOpen) {
+    time = moment().startOf('day');
+    firstOpen = false;
+  } else {
+    time = "01:00 PM"
+  }
+  
+  $(this).data('DateTimePicker').date(time);
+});
